753f6512eb481747e4dd4f797113234609b56776
Bernat <bernat@Bernats-MacBook-Pro.local>
2026-07-14 08:05:15 -0700

    project-created

---
+++ MiDieta/MiDieta/MiDietaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiDietaApp.swift
+//  MiDieta
+//
+//  Created by Bernat on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct MiDietaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
